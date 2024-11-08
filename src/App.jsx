f1f85cc47a39f99898a484c4d6461a5a32c7b31a
Aassdd2121@@ <ahmadiramin817@gmail.com>
2024-11-08 14:37:13 +0200

    catalog page tehty

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
-import Home from './pages/home/Home';
 import Navbar from './components/navbar/Navbar';
-import Header from './components/header/Header';
 import SearchItem from './components/searchItem/SearchItem';
 import Footer from './components/footer/Footer';
 import PopularLocation from './components/popularLocations/PopularLoaction';
 import Hero from './components/hero/Hero';
 import sea from '../public/assets/sea.jpg';
 import Login from '../src/components/login/Login'; // Assuming the Login component is in the same folder
-
+import SignUp from './components/signUp/SignUp';
+import Catalog from './pages/catalog/Catalog';
 function App() {
   return (
     <Router>
@@ -24,7 +23,13 @@ function MainContent() {
   return (
     <>
       {/* Conditionally render Navbar and Header only if the path is not '/login' */}
-      {location.pathname === '/login' && (
+      {location.pathname === '/login' &&  (
+        <>
+          
+        </>
+      )}
+
+      {location.pathname === '/SignUp' &&  (
         <>
           
         </>
@@ -33,22 +38,23 @@ function MainContent() {
       <Routes>
         {/* Login Page Route */}
         <Route path="/login" element={<Login />} />
-
+        <Route path="/SignUp" element={<SignUp />} />
+        <Route path="/Catalog" element={<Catalog />} />
         {/* Other Routes */}
         <Route
           path="/"
           element={
             <>
               <Navbar />
-              <Header />
+              
               <Hero 
                 image={sea}
                 mainHeader="Are you ready for an adventure?" 
                 secondaryHeader="Browse through the popular locations."
               />
-              <PopularLocation />
+              <PopularLocation  path="/Catalog"/>
               <SearchItem />
-              <Home />
+              
               <Footer />
             </>
           }
