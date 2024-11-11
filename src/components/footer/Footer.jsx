e18f2da7fd29b5117affbd47d5d0a6760247fad2
KatjaK <akk45885@edu.redu.fi>
2024-11-11 14:31:07 +0200

    added a footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,39 @@
-import React from 'react'
-import './Footer.css'
+import React from 'react';
+import './Footer.css';
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear(); 
+
   return (
-    <div className='footer'>Footer</div>
-  )
+    <div className='footer grid-container'>
+      <div className="linkit">
+        <p>Footer</p>
+        <a href="#">Kirjaudu</a><br/>
+        <a href="#">Majoitukset</a><br/>
+        <a href="#">Lennot</a>
+      </div>
+      <div className="yhteystiedot">
+        <p>Yhteystiedot</p>
+        <p>Phone: +358 98745632</p>
+     
+      
+      <p>© {currentYear} Your Company. All rights reserved.</p>
+      
+      </div>
+      
+
+
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
+
+
+
+  
+
+ 
+
+ 
+
