be5c1c2361328b0e630a74d7a70c172df1ca177a
Aassdd2121@@ <ahmadiramin817@gmail.com>
2024-11-25 09:56:50 +0200

    navbar on päivitetty

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
-import Navbar from './components/navbar/Navbar';
-import SearchItem from './components/searchItem/SearchItem';
-import Footer from './components/footer/Footer';
-import PopularLocation from './components/popularLocations/PopularLoaction';
-import Hero from './components/hero/Hero';
-import sea from '../public/assets/sea.jpg';
-import Login from '../src/components/login/Login'; // Assuming the Login component is in the same folder
-import SignUp from './components/signUp/SignUp';
+import Login from '../src/pages/login/Login'; 
+import SignUp from '../src/pages/signUp/SignUp';
 import Catalog from './pages/catalog/Catalog';
-import Home from './pages/home/Home';
+import Home from '../src/pages/home/Home';
+// import 'bootstrap/dist/css/bootstrap.css';
 function App() {
   return (
     <Router>
--- frontend/src/components/mailList/MailList.jsx
+++ frontend/src/components/mailList/MailList.jsx
@@ -1,4 +1,4 @@
-import "./mailList.css"
+
 
 const MailList = () => {
   return (
@@ -6,7 +6,7 @@ const MailList = () => {
       <h1 className="mailTitle">Save time, save money!</h1>
       <span className="mailDesc">Sign up and we'll send the best deals to you</span>
       <div className="flex items-center flex-row gap-2">
-        <input className="w-72 h-8 p-2 border-none mr-2 rounded-md" type="text" placeholder="Your Email" />
+        <input className="w-72 h-8 p-2 border-none mr-2 rounded-md  text-black" type="text" placeholder="Your Email" />
         <button className="h-12 w-25 bg-blue-700 text-white font-medium border-none rounded-md cursor-pointer">Subscribe</button>
       </div>
     </div>
