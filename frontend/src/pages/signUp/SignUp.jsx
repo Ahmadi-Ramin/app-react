6e3402405e0bab93bec5bb3cf4b32644d592694d
Aassdd2121@@ <ahmadiramin817@gmail.com>
2024-11-25 10:53:22 +0200

    Login page and sign up page is updated

--- frontend/src/pages/signUp/SignUp.jsx
+++ frontend/src/pages/signUp/SignUp.jsx
@@ -1,4 +1,4 @@
-import './SignUp.css';
+
 import React from 'react';
 
 import Dubai from '../../../public/assets/Dubai.jpg'; // Ensure proper path
@@ -41,32 +41,32 @@ const SignUp = () => {
   };
 
   return (
-    <div className="signup-container">
-      <div className="signup-image">
-        <img src={Dubai} alt="Signup Background" />
+    <div className="relative h-screen w-full">
+      <div className="relative h-full w-full">
+        <img src={Dubai} alt="Signup Background" className='brightness-50 h-full w-full object-cover' />
       </div>
-      <div className="signup-form-container">
-        <h2 className="signup-title">Create an account</h2>
-        <form onSubmit={handleSubmit(onSubmit)} className="signup-form">
+      <div className="h-[450px] w-[400px] bg-white absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg">
+        <h2 className="text-center p-4 font-semibold text-slate-800 text-2xl border-b border-slate-500">Create an account</h2>
+        <form onSubmit={handleSubmit(onSubmit)} className="mt-12 flex flex-col items-center w-full gap-8">
           <Input
-            className="signup-input"
+            className="w-full mx-auto outline-none border border-slate-400 py-1 px-3 rounded-md focus:border-slate-600"
             type="text"
             placeholder="John123"
             register={register('username')}
           />
           <Input
-            className="signup-input"
+            className="w-full mx-auto outline-none border border-slate-400 py-1 px-3 rounded-md focus:border-slate-600"
             type="email"
             placeholder="johndoe@gmail.com"
             register={register('email')}
           />
           <Input
-            className="signup-input"
+            className="w-full mx-auto outline-none border border-slate-400 py-1 px-3 rounded-md focus:border-slate-600"
             type="password"
             placeholder="********"
             register={register('password')}
           />
-          <Button className="signup-button" label="Submit" />
+          <Button className="w-3/4 mt-12 mx-auto cursor-pointer rounded-lg py-2 px-6 text-xl text-white bg-blue-500 transition-all hover:bg-blue-600" label="Submit" />
         </form>
       </div>
     </div>
